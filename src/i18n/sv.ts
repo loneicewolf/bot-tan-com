138a28bf77cef8309ce9fd31ea5f504d35e03c90
0AA01A0F1 <68499986+loneicewolf@users.noreply.github.com>
2026-08-06 11:06:11 +0200

    Improving the "Vad gör hon?" section & the "fritid" section

--- src/i18n/sv.ts
+++ src/i18n/sv.ts
@@ -94,7 +94,7 @@ export const sv: Dictionary = {
         FreeTime: 'Fritid',
         Relax: 'Chillar',
       },
-      statusHeading: 'Vad gör hon?',
+      statusHeading: 'Vad lägger hon sin tid på?',
     },
     energy: {
       label: 'Energi',
